64aff5a3f8177d258a0c7a9fa46aef1a5149cab9
Deepak Kumar Ravi <dpakravi93@gmail.com>
2024-12-31 17:11:43 +0530

    Added some basic aspi functionalities

---
+++ README.md
@@ -0,0 +1,44 @@
+# DeviceHaven  
+
+DeviceHaven is a modern and secure e-commerce platform designed to provide a seamless shopping experience. This project leverages cutting-edge technologies to deliver a user-friendly, scalable, and performance-driven solution.  
+
+---
+
+## 🚀 Live Demo  
+
+Check out the live site: [DeviceHaven](https://lnkd.in/dUkZvgXG)  
+
+---
+
+## 🌟 Features  
+
+### Core Functionalities  
+- **Authentication**: Secure Google Sign-In and OTP verification for email-based sign-in using NextAuth and Nodemailer.  
+- **Recent Searches**: Displays the last 5 searches with dynamic product suggestions as you type.  
+- **Pagination**: Smooth navigation through extensive product catalogs.  
+- **Responsive Design**: Fully optimized for all devices, ensuring a consistent user experience.  
+- **Customer Reviews**: Users can rate products and leave feedback.  
+- **Dark and Light Mode**: Toggle between intuitive dark and light modes to suit user preferences.  
+
+---
+
+## 🔧 Tech Stack  
+
+### Frontend  
+- **Next.js**: Server-side rendering (SSR), static site generation (SSG), and API routes for optimized performance.  
+- **React**: Efficient and dynamic UI components.  
+- **Redux**: Powerful state management for cart functionality, user profiles, and search features.  
+- **Tailwind CSS**: Utility-first CSS framework for fast and responsive design.  
+- **shadcn/ui**: Prebuilt, customizable UI components.  
+- **Framer Motion**: Seamless animations and transitions for a dynamic user experience.  
+
+### Backend  
+- **NextAuth**: Secure and flexible authentication workflows, including Google Sign-In.  
+- **Nodemailer**: Email-based OTP verification for enhanced security.  
+- **MongoDB/Mongoose**: Scalable and efficient database management.  
+- **Stripe**: Integrated for seamless online payment processing.  
+
+### TypeScript  
+- Used across the project to ensure type safety, reliability, and scalability.  
+
+---
---
+++ src/app/api/send-otp/route.ts
@@ -0,0 +1,71 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+import crypto from "crypto";
+import { OTP } from "@/models/otp";
+import { getServerSession } from "next-auth";
+import { mongo } from "mongoose";
+import { mongooseConnect } from "@/lib/mongoose";
+
+const user = process.env.EMAIL;
+const pass = process.env.PASSWORD;
+
+export async function POST(request: Request) {
+
+  try {
+    await mongooseConnect();
+    const { email } = await request.json();
+    console.log("Request Received for email sending");
+
+      if (!email) {
+        return NextResponse.json({ error: "Email is required" }, { status: 400 });
+      }
+
+      // check if an otp has already been generated for the email
+      const existingOtp = await OTP.findOne({ email });
+      if (existingOtp) {
+        console.log("Deleting existing OTP");
+        await OTP.deleteOne({ email }); // 
+      }
+  
+      // Generate a random 6-digit OTP
+      const otp = crypto.randomInt(100000, 999999).toString();
+      const otpExpiry = new Date(Date.now() + 5 * 60 * 1000); 
+
+        // Save the OTP in the database
+        const response = await OTP.create({
+          email,
+          otp,
+          expiry: otpExpiry,
+        });
+    
+    const transporter = nodemailer.createTransport({
+      service: "zoho",
+      host: "smtpro.zoho.in",
+      port: 465,
+      secure: true,
+      auth: {
+        user,
+        pass,
+      },
+    });
+
+    const mailOptions = {
+      from: "device-haven@zohomail.com",
+      to: email,
+      subject: "OTP for account verification",
+      html: `<p>Your OTP is: <strong>${otp}</strong>. It will expire in 5 minutes.</p>`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json(
+      { message: "Message sent successfully",
+        otp: response.otp,
+        expiry: response.expiry,
+       },
+      { status: 200 },
+    );
+  } catch (error) {
+    return new NextResponse("Failed to send message.", { status: 500 })
+  }
+}
