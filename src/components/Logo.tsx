243e7656adf69635f0153902d818da3bc2662d44
Deepak Kumar Ravi <dpakravi93@gmail.com>
2025-01-01 19:53:34 +0530

    Logo changed

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="flex items-center justify-center mb-8">
       <Image
-        src="/WebElectra.png" // Replace with your actual logo path
+        src="/leftLogo.png" // Replace with your actual logo path
         alt="WebElectra Logo"
         width={250}
         height={250}
